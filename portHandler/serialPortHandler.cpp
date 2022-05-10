836d7b11a611fe64cbff67624bf6564bd0d7b01e
Lucas Avila <lucas.soares.avila@gmail.com>
2022-05-10 15:44:45 -0300

    change the workspace

---
+++ portHandler/serialPortHandler.cpp
@@ -0,0 +1,20 @@
+// https://www.boost.org/doc/libs/1_79_0/doc/html/boost_asio.html
+/* 
+- Use the boost library to control the serial ports
+- Use the boost/asio to control the serial ports
+*/
+/*
+- if serial port is on -> list
+- find the serial port that use the arduino board (see Arduino IDE code)  
+- return the serial port
+*/
+
+// ---------------------------------------------------------- //
+// https://www.mvtec.com/doc/halcon/13/en/open_serial.html
+/*
+- Use the serial port handler internal of the c++ (c++11 +)
+*/
+
+// #include <Serial.h>
+
+
